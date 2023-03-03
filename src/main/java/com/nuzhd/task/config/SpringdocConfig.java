aa0f31c33692bc8f1d74a02757f037dab334d3a6
yeeeip <dmit.nujdin2014@yandex.ru>
2023-03-04 00:03:43 +0300

    Added swagger

---
+++ src/main/java/com/nuzhd/task/config/SpringdocConfig.java
@@ -0,0 +1,19 @@
+package com.nuzhd.task.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+public class SpringdocConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .info(new Info().title("Student Management System Documentation").version("1.0.0"));
+    }
+
+}
